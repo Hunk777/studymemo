889d3c058600d8c025c43a7474f80dee0498fcf8
pilumu <agli.hunk@gmail.com>
2024-04-12 09:29:20 -0400

    third

--- 2.py
+++ 2.py
@@ -12,11 +12,13 @@ result1 = judge(87)
 result2 = judge(3e3)
 result3 = judge(123)
 result4 = judge(94)
+result5 = judge(4e2)
 
 print(result1)
 print(result2)
 print(result3)
 print(result4)
+print(result5)
 #AAA
 
 
